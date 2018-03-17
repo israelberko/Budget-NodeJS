5f25b5d370b91f25b2c061c162a212de55060cb5
israelb <israel.berko@cellwize.com>
2018-03-17 15:44:08 +0200

    showing income and expense items

---
+++ models/budget/income.js
@@ -0,0 +1,8 @@
+var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+
+module.exports = Income;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var expense = require('/Users/israelb/Developer tools/learning-Nodejs/budget-handlebars/public/javascripts/expense');
 var budget = require('/Users/israelb/Developer tools/learning-Nodejs/budget-handlebars/public/javascripts/budget');
+var BudgetDetails = require('../models/BudgetDetails');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,11 +13,13 @@ router.get('/', function(req, res, next) {
 
 /* Post home page. */
 router.post('/', function(req, res, next) {
-    console.log(req.body.add__value);
-    var budgetController = new budget();
-    budgetController.addItem(req.body.add__type, req.body.add__description, req.body.add__value);
-    console.log(`budget is : ${budgetController.testing()}`);
-    res.render('index', { title: 'cool', condition: true, anyArray: [1, 2, 3] });
+    var budgetDetails = new BudgetDetails(req.session.budgetDetails ? req.session.budgetDetails : {});
+
+    budgetDetails.addItem(req.body.add__type, req.body.add__description, req.body.add__value);
+    req.session.budgetDetails = budgetDetails;
+    var incomes = budgetDetails.generateArray(budgetDetails.data.allItems.inc);
+    var expenses = budgetDetails.generateArray(budgetDetails.data.allItems.exp);
+    res.render('index', { title: 'cool', condition: true, incomes: incomes, expenses: expenses });
 });
 
 module.exports = router;
